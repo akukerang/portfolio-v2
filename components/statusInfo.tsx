73c2a5553b5af2f02c4b9d66fa2cad12668aecbf
gabe suoth <gabrielsuoth@gmail.com>
2025-04-11 14:19:31 -0400

    usage bar and updates

--- components/statusInfo.tsx
+++ components/statusInfo.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Uptime from "./uptime";
 
 interface StatusItemProps {
@@ -23,16 +23,19 @@ const StatusItem = ({ pid, task, status, cpu, mem, info }: StatusItemProps) => {
     );
 }
 
-const BarChart = ({ name, usage }: { name: string, usage: number }) => {
+const BarChart = ({ name, usage, max }: { name: string, usage: number, max: number }) => {
     const totalBlocks = 15;
-    const filledBlocks = Math.round((usage / 100) * totalBlocks);
+    const percent = usage / max;
+    const filledBlocks = Math.round(percent * totalBlocks);
+    console.log(usage, max, percent, filledBlocks);
+
     const emptyBlocks = totalBlocks - filledBlocks;
     const bar =
         "█".repeat(filledBlocks) + "-".repeat(emptyBlocks);
 
     return (
         <div>
-            {`${name}: [${bar}] ${usage}%`}
+            {`${name}: [${bar}] ${(percent * 100).toFixed(2)}%`}
         </div>
     );
 
@@ -43,17 +46,49 @@ const BarChart = ({ name, usage }: { name: string, usage: number }) => {
 const StatusInfo = () => {
     const [cpuUsage, setCPUUsage] = useState(0)
     const [memoryUsage, setMemoryUsage] = useState(0)
+    const [memoryValues, setMemoryValues] = useState<number[]>([0, 0, 0, 0]);
+    const [cpuValues, setCPUValues] = useState<number[]>([0, 0, 0, 0]);
+
+    const getRandomValue = (min: number, max: number) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const cpuValues =
+                [
+                    getRandomValue(10, 40)
+                    , getRandomValue(3, 10)
+                    , getRandomValue(4, 20)
+                    , getRandomValue(10, 30)
+                ]
+            const memoryValues =
+                [
+                    getRandomValue(400, 1300)
+                    , getRandomValue(150, 600)
+                    , getRandomValue(100, 500)
+                    , getRandomValue(50, 500)
+                ]
+            setCPUValues(cpuValues);
+            setMemoryValues(memoryValues);
+            setCPUUsage(cpuValues.reduce((a, b) => a + b, 0));
+            setMemoryUsage(memoryValues.reduce((a, b) => a + b, 0));
+        }, 4000);
+        return () => clearInterval(interval); // cleanup
+    }, []);
+
+
 
     return (
-        <div className="border-2 border-gray-300 rounded-sm py-4 w-full">
+        <div className="mt-2 border-2 border-gray-300 rounded-sm py-4 w-full">
             <div className="flex flex-row justify-between px-4 ">
                 <h1>System Status: gabeOS</h1>
                 <Uptime />
             </div>
             <div className="flex flex-row gap-8 mt-1 px-4">
                 {/* Bar Chart */}
-                <BarChart name={"CPU"} usage={cpuUsage} />
-                <BarChart name={"RAM"} usage={memoryUsage} />
+                <BarChart name={"CPU"} usage={cpuUsage} max={100} />
+                <BarChart name={"MEM"} usage={memoryUsage} max={16384} />
 
             </div>
             <div>
@@ -66,10 +101,10 @@ const StatusInfo = () => {
                     <h1 className="w-[10%]">MEM</h1>
                     <h1 className="w-[33%]">INFO</h1>
                 </div>
-                <StatusItem pid="001" task="work" status="RUNNING" cpu={20} mem={40} info="Testing" />
-                <StatusItem pid="002" task="location" status="IDLE" cpu={20} mem={40} info="Testing" />
-                <StatusItem pid="003" task="now-playing" status="RUNNING" cpu={20} mem={128} info="Testing" />
-                <StatusItem pid="004" task="last-commit" status="IDLE" cpu={20} mem={40} info="Testing" />
+                <StatusItem pid="001" task="work" status="RUNNING" cpu={cpuValues[0]} mem={memoryValues[0]} info="Testing" />
+                <StatusItem pid="002" task="location" status="IDLE" cpu={cpuValues[1]} mem={memoryValues[1]} info="Testing" />
+                <StatusItem pid="003" task="now-playing" status="RUNNING" cpu={cpuValues[2]} mem={memoryValues[2]} info="Testing" />
+                <StatusItem pid="004" task="last-commit" status="IDLE" cpu={cpuValues[3]} mem={memoryValues[3]} info="Testing" />
 
 
 
