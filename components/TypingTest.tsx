93ee9a6f8f355000075c95bc237ffe6a097eb473
gabe suoth <gabrielsuoth@gmail.com>
2025-04-14 17:18:07 -0400

    initial layout

--- components/TypingTest.tsx
+++ components/TypingTest.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
+
+interface ResultProps {
+    wpm: number;
+    acc: number;
+}
+
+const ResultScreen: React.FC<ResultProps> = ({ wpm, acc }) => {
+    return <></>
+}
+
+
 const TypingTest = () => {
+    const [gameStart, setGameStart] = useState(false);
+    const [wpm, setWPM] = useState(0);
+    const [acc, setAcc] = useState(0);
+    const [timer, setTimer] = useState(30);
     return (
-        <div>
-            <h1>Test</h1>
+        <div className="mt-2 border-2 border-[var(--color_08)] rounded-sm w-full h-4/5 flex p-2 ">
+            <h1 className="text-2xl color-4 mx-auto">Typing Test</h1>
+            <div>
+                {/* Timer Here hidden till game start*/}
+            </div>
+            <div>
+                {/* Test and Text Here */}
+                {/* 3 Lines of Text Maximum */}
+            </div>
         </div>
     );
 };
