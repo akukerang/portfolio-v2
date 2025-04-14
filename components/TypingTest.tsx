f32fe283cb339c2af8150046e74610a433683a5c
gabe suoth <gabrielsuoth@gmail.com>
2025-04-14 17:36:49 -0400

    layout stuff, test paragraph

--- components/TypingTest.tsx
+++ components/TypingTest.tsx
@@ -1,5 +1,6 @@
+"use client";
 import { useState } from "react";
-
+import paragraphs from "@/data/paragraph.json";
 
 interface ResultProps {
     wpm: number;
@@ -16,17 +17,27 @@ const TypingTest = () => {
     const [wpm, setWPM] = useState(0);
     const [acc, setAcc] = useState(0);
     const [timer, setTimer] = useState(30);
+    const [paragraph, setParagraph] = useState<string>(paragraphs.paragraphs[Math.floor(Math.random() * paragraphs.paragraphs.length)]);
+
+
+
     return (
-        <div className="mt-2 border-2 border-[var(--color_08)] rounded-sm w-full h-4/5 flex p-2 ">
+        <div className="mt-2 border-2 border-[var(--color_08)] rounded-sm w-full h-4/5 flex p-8 flex-col">
             <h1 className="text-2xl color-4 mx-auto">Typing Test</h1>
-            <div>
-                {/* Timer Here hidden till game start*/}
-            </div>
-            <div>
-                {/* Test and Text Here */}
-                {/* 3 Lines of Text Maximum */}
+            <div className="overflow-hidden text-xl mx-16 mb-8">
+                <h1 className="text-2xl color-7 mx-auto">{timer}</h1>
+                <div
+                    className="overflow-hidden whitespace-pre-wrap"
+                    style={{
+                        display: "-webkit-box",
+                        WebkitBoxOrient: "vertical",
+                        WebkitLineClamp: 3,
+                    }}
+                >
+                    {paragraph}
+                </div>
             </div>
         </div>
     );
-};
+}
 export default TypingTest;
