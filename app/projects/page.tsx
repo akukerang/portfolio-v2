04152b8651e0bdf73e5ec5b279b6f23d8bb50573
gabe suoth <gabrielsuoth@gmail.com>
2025-04-30 21:54:06 -0400

    projectlist revalidate

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ProjectList from "@/components/projectList";
 import Command from "@/components/command";
+export const revalidate = 0; // Revalidate on every request
+
 const Projects = async () => {
   return (
     <div>
